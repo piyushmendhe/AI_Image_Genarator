2e652f91c94f5d21517a973adf2b3e0bf0a68a37
piyushmendhe <mendhe.piyush31@gmail.com>
2024-02-02 23:06:45 +0530

    Created form fild

--- frontEnd/src/pages/CreatePost.jsx
+++ frontEnd/src/pages/CreatePost.jsx
@@ -15,9 +15,44 @@ const CreatePost = () => {
   const[generatingImg,setGeneratingImg] = useState(false);
   const[loading ,setLoading] = useState(false);
 
-
+  const handleSubmit=() =>{}
+  const handleChange=(e) =>{}
+  const handleSurpriseMe = ()=>{}
   return (
-    <div>CreatePost</div>
+    <section className='max-w-7xl mx-auto'>
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">
+          Create
+        </h1>
+          <p className="mt-2 text-[#666e75] text-[16px] maxw[500px]">
+            Create AI-Image
+          </p>
+      </div>
+
+      <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+      <div className='flex flex-col gap-5'>
+        <Formfield
+        lableName="Your Name"
+        type = "text"
+        name="name"
+        placeholder="Piyush Mendhe"
+        value={form.name}
+        handleChange={handleChange}
+        />
+        <Formfield
+        lableName="Promt"
+        type = "text"
+        name="Promt"
+        placeholder="Piyush Mendhe"
+        value={form.prompt}
+        handleChange={handleChange}
+        isSurpriseMe
+        handleSurpriseMe ={handleSurpriseMe}
+        />
+      </div>
+      </form>
+
+    </section>
   )
 }
 
